a17659b57e7a8f9b8deb077101b5f7c59974ba11
Babs Seed <babs@Manehattan>
2015-01-09 13:24:22 +0100

    First!

---
+++ Makefile
@@ -0,0 +1,50 @@
+SPARROW_FOLDER = ../sparrow3d
+
+#==stuff linked to
+STATIC = -Wl,-rpath=$(SPARROW_FOLDER) -Wl,-Bstatic -lsparrow3d -Wl,-Bdynamic
+DYNAMIC = -lSDL_ttf -lSDL_mixer -lSDL_image -lSDL -lm
+#==global Flags. Even on the gp2x with 16 kb Cache, -O3 is much better then -Os
+CFLAGS = -fsingle-precision-constant -fPIC
+# Testtweaks: -fgcse-lm -fgcse-sm -fsched-spec-load -fmodulo-sched -funsafe-loop-optimizations -Wunsafe-loop-optimizations -fgcse-las -fgcse-after-reload -fvariable-expansion-in-unroller -ftracer -fbranch-target-load-optimize
+GENERAL_TWEAKS = -ffast-math
+#==PC==
+FLAGS = -g -DDESKTOP $(GENERAL_TWEAKS)
+SDL = `sdl-config --cflags`
+
+ifdef TARGET
+include $(SPARROW_FOLDER)/target-files/$(TARGET).mk
+BUILD = ./build/$(TARGET)/hase_sprite_tester
+SPARROW_LIB = $(SPARROW_FOLDER)/build/$(TARGET)/sparrow3d
+ifeq ($(TARGET),win32)
+	#the BUILDING_DLL is important for the linking...
+	STATIC = $(SPARROW_LIB)/libsparrowNet_static.a -DBUILDING_DLL
+endif
+else
+TARGET = "Default (change with make TARGET=otherTarget. See All targets with make targets)"
+BUILD = .
+SPARROW_LIB = $(SPARROW_FOLDER)
+endif
+LIB += -L$(SPARROW_LIB)
+INCLUDE += -I$(SPARROW_FOLDER)
+
+CFLAGS += $(FLAGS) $(PARAMETER)
+
+all: hase_sprite_tester
+	@echo "=== Built for Target "$(TARGET)" ==="
+
+targets:
+	@echo "The targets are the same like for sparrow3d. :P"
+
+hase_sprite_tester: hase_sprite_tester.c makeBuildDir
+	$(CC) $(CFLAGS) hase_sprite_tester.c $(SDL) $(INCLUDE) $(LIB) $(STATIC) $(DYNAMIC) -o $(BUILD)/hase_sprite_tester$(SUFFIX)
+
+makeBuildDir:
+	 @if [ ! -d $(BUILD:/hase_sprite_tester=/) ]; then mkdir $(BUILD:/hase_sprite_tester=/);fi
+	 @if [ ! -d $(BUILD) ]; then mkdir $(BUILD);fi
+
+clean:
+	rm -f *.o
+	rm -f hase_sprite_tester
+
+oclean:
+	rm -f *.o
---
+++ hase_sprite_tester.c
@@ -0,0 +1,132 @@
+ /* This file is part of hase_sprite_tester.
+  * hase_sprite_tester is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  * 
+  * hase_sprite_tester is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  * 
+  * You should have received a copy of the GNU General Public License
+  * along with hase_sprite_tester.  If not, see <http://www.gnu.org/licenses/>
+  * 
+  * For feedback and questions about my Files and Projects please mail me,
+  * Alexander Matthes (Ziz) , zizsdl_at_googlemail.com */
+
+#include <sparrow3d.h>
+
+SDL_Surface* screen;
+spSpritePointer sprite;
+int counter = 0;
+int rad = 0;
+
+void draw()
+{
+	int max_size = screen->h*3/4;
+	spClearTarget( spGetHSV(counter*64,127,127) );
+	spEllipseBorder(screen->w/2,screen->h/2,0,max_size/3+2,max_size/3+2,max_size/30,max_size/30,65535);
+	
+	sprite->rotation = 0;
+	
+		sprite->zoomX = sprite->zoomY = SP_ONE;
+		spDrawSprite(screen->w/2-max_size/13,screen->h/2,0,sprite);
+		
+		sprite->zoomX = sprite->zoomY = SP_ONE/2;
+		spDrawSprite(screen->w/2-max_size/5,screen->h/2,0,sprite);
+		
+		sprite->zoomX = sprite->zoomY = SP_ONE*2;
+		spDrawSprite(screen->w/2+max_size/7,screen->h/2,0,sprite);
+	
+	sprite->rotation = -rad+SP_PI/2;
+	
+		int r = max_size/3 + 16 + (abs(spSin(-rad*6)) * 16 >> SP_ACCURACY);
+		int x = spCos(-rad) * r >> SP_ACCURACY;
+		int y = spSin(-rad) * r >> SP_ACCURACY;
+		sprite->zoomX = sprite->zoomY = SP_ONE;
+		spDrawSprite(screen->w/2+x,screen->h/2+y,0,sprite);
+	
+	sprite->rotation = -rad+SP_PI*2/3+SP_PI/2;
+		
+		r = max_size/3 + 8 + (abs(spSin((-rad+SP_PI*2/3)*6)) * 8 >> SP_ACCURACY);
+		x = spCos(-rad+SP_PI*2/3) * r >> SP_ACCURACY;
+		y = spSin(-rad+SP_PI*2/3) * r >> SP_ACCURACY;
+		sprite->zoomX = sprite->zoomY = SP_ONE/2;
+		spDrawSprite(screen->w/2+x,screen->h/2+y,0,sprite);
+		
+	sprite->rotation = -rad-SP_PI*2/3+SP_PI/2;
+		
+		r = max_size/3 + 32 + (abs(spSin((-rad-SP_PI*2/3)*6)) * 32 >> SP_ACCURACY);
+		x = spCos(-rad-SP_PI*2/3) * r >> SP_ACCURACY;
+		y = spSin(-rad-SP_PI*2/3) * r >> SP_ACCURACY;
+		sprite->zoomX = sprite->zoomY = SP_ONE*2;
+		spDrawSprite(screen->w/2+x,screen->h/2+y,0,sprite);
+		
+	spFlip();
+}
+
+int jump = -200;
+
+int calc(Uint32 steps)
+{
+	counter += steps;
+	int i;
+	for (i = 0; i < steps; i++)
+	{
+		jump++;
+		if (jump <= 0)
+			spUpdateSprite(sprite,1);
+		else
+			rad += 128;
+		if (jump > SP_PI/6/128)
+			jump = -200;
+		if (rad >= 2*SP_PI)
+		{
+			jump = -200;
+			rad = 0;
+		}
+	}
+	if (spGetInput()->button[SP_BUTTON_START])
+		return 1;
+	if (spGetInput()->button[SP_BUTTON_SELECT])
+		return 1;
+	return 0;
+}
+
+void resize(Uint16 w,Uint16 h)
+{
+	spSelectRenderTarget(spGetWindowSurface());
+}
+
+int main(int argc, char **argv)
+{
+	if (argc <= 1)
+	{
+		printf("Usage: hase_sprite_tester image\n");
+		return 1;
+	}
+	spSetDefaultWindowSize(320,320);
+	spInitCore();
+	spSetAffineTextureHack(0); //We don't need it :)
+	spInitMath();
+	screen = spCreateDefaultWindow();
+	resize(screen->w,screen->h);
+	spSetZSet(0);
+	spSetZTest(0);
+	
+	SDL_Surface* image = spLoadSurface( argv[1] );
+	if (image == NULL)
+	{
+		printf("Couldn't load %s\n",argv[1]);
+		return 1;
+	}
+	
+	sprite = spNewSprite("hase");
+	spNewSubSpriteTilingRow( sprite, image,   0, 0, 34, 34, 34, 34, 4, 40);
+	spNewSubSpriteTilingRow( sprite, image, 136, 0, 34, 34, 34, 34, 2, 20);
+	spLoop( draw, calc, 10, resize, NULL );
+	spDeleteSprite(sprite);
+	spQuitCore();
+	return 0;
+}
